2b8aa3610d5e87ad97c289a702ca07a1fd70508f
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-12 09:19:21 +0500

    3 entities created

--- HomeAccountingMVC/Entities/Category.cs
+++ HomeAccountingMVC/Entities/Category.cs
@@ -1,10 +1,18 @@
-﻿namespace HomeAccountingMVC.Entities
+﻿using HomeAccountingMVC.Enums.CategoryEnums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeAccountingMVC.Entities
 {
     public class Category
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public Income Income { get; set; }
+        public Outcome Outcome { get; set; }
+        public int TransactionID { get; set; }
 
+        [ForeignKey(nameof(TransactionID))]
+        public Transaction Transaction { get; set; }
     }
 }
--- HomeAccountingMVC/Entities/Transaction.cs
+++ HomeAccountingMVC/Entities/Transaction.cs
@@ -1,6 +1,13 @@
-﻿namespace HomeAccountingMVC.Entities
+﻿using HomeAccountingMVC.Enums.TransactionEnums;
+
+namespace HomeAccountingMVC.Entities
 {
     public class Transaction
     {
+        public int ID { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public TType Type { get; set; }
+        public Account Account { get; set; }
+        public Category Category { get; set; }
     }
 }
