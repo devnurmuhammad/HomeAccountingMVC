78ee0978bc2237d5ff2099ac5b1837b8ff309c0d
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-12 09:19:08 +0500

    AppDbContext added

---
+++ HomeAccountingMVC/Db Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HomeAccountingMVC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeAccountingMVC.Db_Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
