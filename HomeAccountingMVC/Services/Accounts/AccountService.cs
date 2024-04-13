f6b09f8a045af3a2ba72312d90c2910288b2841b
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-13 15:54:51 +0500

    AccountService added

---
+++ HomeAccountingMVC/Services/Accounts/AccountService.cs
@@ -0,0 +1,54 @@
+﻿using HomeAccountingMVC.DTOs;
+using HomeAccountingMVC.Entities;
+using HomeAccountingMVC.Models;
+using HomeAccountingMVC.Repositories.Accounts;
+
+namespace HomeAccountingMVC.Services.Accounts
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repository;
+
+        public AccountService(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool Create(CreateAccountViewModel accountDTO)
+        {
+            Account account = new Account()
+            {
+                Firstname = accountDTO.Firstname,
+                Email = accountDTO.Email,
+                Username = accountDTO.Username,
+                Password = accountDTO.Password,
+            };
+
+            bool result = _repository.Create(account);
+         
+            return result;
+        }
+
+        public bool Delete(string username, AccountDTO accountDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<Account>> GetAllAsync()
+        {
+            IList<Account> accounts = await _repository.GetAllAsync();
+
+            return accounts;
+        }
+
+        public Account GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account Update(string username, AccountDTO accountDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeAccountingMVC/Services/Accounts/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using HomeAccountingMVC.DTOs;
+using HomeAccountingMVC.Entities;
+using HomeAccountingMVC.Models;
+
+namespace HomeAccountingMVC.Services.Accounts
+{
+    public interface IAccountService
+    {
+        bool Create(CreateAccountViewModel viewModel);
+        Task<IList<Account>> GetAllAsync();
+        Account Update(string username, AccountDTO accountDTO);
+        bool Delete(string username, AccountDTO accountDTO);
+        Account GetByUsername(string username);
+    }
+}
