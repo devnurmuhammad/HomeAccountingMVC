13f758a755a54bdf9db8b191e36449f7b076bed3
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-13 15:54:13 +0500

    CreateAccountViewModel created

---
+++ HomeAccountingMVC/Models/CreateAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeAccountingMVC.Models
+{
+    public class CreateAccountViewModel
+    {
+        public string? Firstname { get; set; }
+        [Required]
+        public required string Username { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        public string? Email { get; set; }
+    }
+}
