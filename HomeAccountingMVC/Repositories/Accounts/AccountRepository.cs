43a31f9fed5859cd665fdde932e1a1461a82e21b
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-13 15:54:33 +0500

    AccountRepositories created

---
+++ HomeAccountingMVC/Repositories/Accounts/AccountRepository.cs
@@ -0,0 +1,60 @@
+﻿using HomeAccountingMVC.Db_Context;
+using HomeAccountingMVC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeAccountingMVC.Repositories.Accounts
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(Account account)
+        {
+            _context.Accounts.Add(account);
+            int result = _context.SaveChanges();
+
+            return result > 0;
+        }
+
+        public bool Delete(string username)
+        {
+            Account? account = _context.Accounts.FirstOrDefault(x => x.Username == username);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IList<Account>> GetAllAsync()
+        {
+            IList<Account> accounts = await _context.Accounts.ToListAsync();
+
+            return accounts;
+        }
+
+        public Account GetByUsername(string username)
+        {
+            Account? account = _context.Accounts.FirstOrDefault(x => x.Username == username);
+            if (account != null)
+            {
+                return account;
+            }
+            throw new Exception("Not f");
+        }
+
+        public Account Update(Account account)
+        {
+            _context.Accounts.Update(account);
+            _context.SaveChanges();
+            return account;
+        }
+    }
+}
---
+++ HomeAccountingMVC/Repositories/Accounts/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using HomeAccountingMVC.Entities;
+
+namespace HomeAccountingMVC.Repositories.Accounts
+{
+    public interface IAccountRepository
+    {
+        bool Create(Account account);
+        Task<IList<Account>> GetAllAsync();
+        Account Update(Account account);
+        bool Delete(string username);
+        Account GetByUsername(string username);
+    }
+}
