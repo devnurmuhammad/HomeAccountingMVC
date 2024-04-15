cd0ff3a826df7bb83cf1733815ea44031aab5680
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-15 16:05:00 +0500

    AccountViews done

--- HomeAccountingMVC/Program.cs
+++ HomeAccountingMVC/Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Accounting}/{action=GetAccounts}/{id?}");
 
 app.Run();
