3f0e324ff5874c0a8a9a121dc6f4ca236135d152
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-04-23 19:15:32 +0500

    TransactionController is added

---
+++ HomeAccountingMVC/Controllers/TransactionController.cs
@@ -0,0 +1,34 @@
+﻿using HomeAccountingMVC.Models;
+using HomeAccountingMVC.Services.Transactions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAccountingMVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionService _service;
+
+        public TransactionController(ITransactionService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTransactionViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Create(model);
+                return View(model);
+            }
+
+            throw new Exception("not valid");
+        }
+    }
+}
