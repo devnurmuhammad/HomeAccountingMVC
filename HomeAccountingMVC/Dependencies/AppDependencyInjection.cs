97ce6178a4fd200e5f0503e761e162aa5e71cdee
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-05-02 19:07:37 +0500

    DependencyInjection created

---
+++ HomeAccountingMVC/Dependencies/AppDependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using HomeAccountingMVC.Repositories.Accounts;
+using HomeAccountingMVC.Repositories.Categories;
+using HomeAccountingMVC.Repositories.Transactions;
+using HomeAccountingMVC.Services.Accounts;
+using HomeAccountingMVC.Services.Categories;
+using HomeAccountingMVC.Services.Transactions;
+
+namespace HomeAccountingMVC.Dependencies
+{
+    public static class AppDependencyInjection
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IAccountRepository, AccountRepository>()
+                .AddScoped<IAccountService, AccountService>()
+                .AddScoped<ITransactionRepository, TransactionRepository>()
+                .AddScoped<ITransactionService, TransactionService>()
+                .AddScoped<ICategoryRepository, CategoryRepository>()
+                .AddScoped<ICategoryService, CategoryService>();
+
+            return services;
+        }
+    }
+}
